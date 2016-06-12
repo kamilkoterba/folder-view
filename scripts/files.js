656a4462f263c738bef628f1a46b0f4436b36e61
Kamil Koterba <kamil@wikia-inc.com>
2016-06-12 10:07:36 +0200

    Rename form

--- scripts/files.js
+++ scripts/files.js
@@ -1,7 +1,15 @@
 (function () {
   'use strict';
 
+  var FilesModel = Backbone.Model.extend({
+    rename: function () {
+      this.trigger('rename', this, this.collection);
+    },
+  });
+
   var FilesList = Backbone.Collection.extend({
+    model: FilesModel,
+
     marked: function () {
       return this.where({
         marked: true,
@@ -13,25 +21,49 @@
 
   var FileView = Backbone.View.extend({
     events: {
+      'click .cancel': 'cancel',
+      'click .save': 'save',
       'click .item-check': 'markToggle',
     },
     tagName: 'li',
     template: _.template(
       $('#fileTemplate').html()
     ),
+
     initialize: function () {
       this.listenTo(this.model, 'destroy', this.remove);
+      this.listenTo(this.model, 'rename', this.rename);
     },
 
-    render: function () {
-      this.$el.html(
-        this.template(this.model.toJSON())
-      );
-      return this;
+    cancel: function () {
+      this.$el.removeClass('renaming');
+      this.$('.rename-filename').attr('value', '');
     },
 
     markToggle: function () {
       this.model.set('marked', !this.model.get('marked'));
+    },
+
+    rename: function () {
+      if (this.model.attributes.marked) {
+        this.$el.addClass('renaming');
+        this.$('.rename-box').focus();
+        this.$('.rename-filename').attr('value', this.model.attributes.name);
+      }
+    },
+
+    render: function () {
+      var templateData = this.model.toJSON();
+
+      templateData.checked = templateData.marked ? 'checked="checked"' : '';
+      this.$el.html(this.template(templateData));
+      return this;
+    },
+
+    save: function () {
+      this.$el.removeClass('renaming');
+      this.model.set('name', this.$('.rename-filename').val());
+      this.render();
     },
   });
 
@@ -39,11 +71,19 @@
     el: document.getElementById('folderView'),
     events: {
       'click #delete': 'delete',
+      'click #rename': 'rename',
     },
 
     initialize: function () {
-      filesList;
       this.listenTo(filesList, 'add', this.addOne);
+    },
+
+    addOne: function (file) {
+      var fileView = new FileView({
+        model: file,
+      });
+
+      this.$('#fileList').append(fileView.render().el);
     },
 
     delete: function () {
@@ -54,14 +94,13 @@
       return false;
     },
 
-    addOne: function (file) {
-      var data = file.toJSON();
-      var fileView = new FileView({
-        model: file,
-      });
-      this.$('#fileList').append(fileView.render().el);
+    rename: function () {
+      _.invoke(filesList.marked(), 'rename');
+
+      return false;
     },
   });
+
   var App = new AppView;
 
   filesList.add([
